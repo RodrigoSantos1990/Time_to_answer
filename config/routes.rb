50aff5c6441163d6c336774580c861d79a48d1bc
RodrigoSantos1990 <rodrigo.santos@monde.com.br>
2019-11-25 15:55:12 -0300

    Admins com funcionalidades editar, atualizar, mostrar

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit, :update]
+    resources :admins
   end
   devise_for :admins
 
